ae6eb8ed9829f5bbce933aab6f0a46e643bf501b
Samuel Fernandez <samu.fm05@gmail.com>
2025-12-13 19:39:24 -0300

    Creado rama search-view

--- src/routes/RoutesPrincipal.jsx
+++ src/routes/RoutesPrincipal.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import HomeScreen from "../views/HomeScreen";
 import ProtectedRoutes from "./ProtectedRoutes";
 import PagesLayout from "../layout/PagesLayout";
+import SearchScreen from "../views/SearchScreen";
 
 const RoutesPrincipal = () => {
   return (
     <Routes>
       <Route path="/" element={<PagesLayout />}>
         <Route index element={<HomeScreen />} />
+        <Route path="search" element={<SearchScreen />} />
       </Route>
     </Routes>
   );
---
+++ src/views/SearchScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchScreen = () => {
+  return (
+    <div className="search-screen py-3">
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default SearchScreen;
