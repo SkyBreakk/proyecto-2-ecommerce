c1b2930035b5f5b3bfb8eb018e802d5b11c5d6cb
Samuel Fernandez <samu.fm05@gmail.com>
2025-12-13 19:08:31 -0300

    Diseñado la página principal, falta añadir links a los productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesPrincipal from "./routes/RoutesPrincipal";
+import "./assets/css/root.css";
 
 function App() {
   return (
--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import listaProductos from "../data/products.json";
+import "../assets/css/homepage.css";
+import ListProductCards from "../components/HomeScreen/ListProductCards";
+import CarouselHome from "../components/HomeScreen/CarouselHome";
 
 const HomeScreen = () => {
+  function conseguirProductos() {
+    const productosGuardados = localStorage.getItem("productos");
+    return productosGuardados ? JSON.parse(productosGuardados) : listaProductos;
+  }
+  const productos = conseguirProductos();
+
   return (
-    <div>
-      <h1>HomeScreen</h1>
+    <div className="home-view py-3">
+      <div className="container">
+        {/* Carousel */}
+        <div className="row row-cols-1">
+          <CarouselHome lista={productos} />
+        </div>
+        {/* Lista rating */}
+        <div className="row flex-column">
+          <ListProductCards lista={productos} orden={"rating"} />
+        </div>
+        {/* Lista precio */}
+        <div className="row flex-column">
+          <ListProductCards lista={productos} orden={"precio"} />
+        </div>
+      </div>
     </div>
   );
 };
