dda38afcfd6f196e9d8d38cf80e60380bda56e62
Samuel Fernandez <samu.fm05@gmail.com>
2025-12-13 19:49:30 -0300

    Movido el navbar del homescreen a pageslayout

--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import NavBar from "../components/navbar/NavBar"
 
 const HomeScreen = () => {
-  return (
-    <NavBar />
-  );
+  return <div>HomeScreen</div>;
 };
 
 export default HomeScreen;
