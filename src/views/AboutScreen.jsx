4cee72d81464f1092eed022f2418d7bf9bcd171a
LOPEZ RUBEN <rubenlopezrolando@gmail.com>
2025-12-16 21:17:42 -0300

    cargando arreglo de about

--- src/views/AboutScreen.jsx
+++ src/views/AboutScreen.jsx
@@ -1,10 +1,5 @@
 import "../assets/css/about.css";
-
-// IMPORT DE IMÁGENES
-import rubenImg from "../assets/img/about/ruben.jpg";
-import samuelImg from "../assets/img/about/samuel.jpg";
-import mariaImg from "../assets/img/about/maria.jpg";
-import pabloImg from "../assets/img/about/pablo.jpg";
+import team from "../data/team";
 
 function AboutScreen() {
   return (
@@ -30,125 +25,31 @@ function AboutScreen() {
 
       {/* INTEGRANTES */}
       <section className="about-team row">
+        {team.map((member, index) => (
+          <div key={index} className="col-md-3 col-sm-6 team-card">
+            <div className="team-box">
 
-        {/* INTEGRANTE 1 */}
-        <div className="col-md-3 col-sm-6 team-card">
-        <div div className="team-box">
+              <img
+                src={member.img}
+                alt={member.name}
+                className="team-photo"
+              />
 
-        <img
-        src={rubenImg}
-        alt="Ruben López"
-        className="team-photo"
-        />
+              <h4>{member.name}</h4>
+              <span className="team-role">{member.role}</span>
 
-        <h4>Ruben López</h4>
-        <span className="team-role">
-      Scrum Master · Frontend Developer
-        </span>
+              <div className="team-contribution">
+                <span>Mi aporte fue:</span>
+                <ul>
+                  {member.contributions.map((item, i) => (
+                    <li key={i}>{item}</li>
+                  ))}
+                </ul>
+              </div>
 
-        
-
-        <div className="team-contribution">
-        <span>Mi aporte fue:</span>
-        <ul>
-            <li>Scrum Master</li>
-            <li>Encargado del tabler de Trello</li>
-            <li>Navbar</li>
-            <li>Footer</li>
-            <li>Acerca de nosotros</li>
-            <li>...</li>
-        </ul>
-        </div>
-
-        </div>
-        </div>
-
-
-        {/* INTEGRANTE 2 */}
-        <div className="col-md-3 col-sm-6 team-card">
-        <div div className="team-box">
-
-        <img
-        src={samuelImg}
-        alt="Samuel Fernandez"
-        className="team-photo"
-        />
-
-        <h4>Samuel Fernandez</h4>
-        <span className="team-role">
-      Encargado Tecnico · Frontend Developer
-        </span>
-
-        <div className="team-contribution">
-        <span>Mi aporte fue:</span>
-        <ul>
-            <li>Encargado Tecnico</li>
-            <li>Maqueteador de la web</li>
-            <li>Pagina Principal</li>
-            <li>Buscador</li>
-            <li>...</li>
-        </ul>
-        </div>
-
-        </div>
-        </div>
-
-        {/* INTEGRANTE 3 */}
-        <div className="col-md-3 col-sm-6 team-card">
-        <div div className="team-box">
-
-        <img
-        src={mariaImg}
-        alt="Maria Morales"
-        className="team-photo"
-        />
-
-        <h4>Maria Morales</h4>
-        <span className="team-role">
-            Frontend Developer
-        </span>
-
-        <div className="team-contribution">
-        <span>Mi aporte fue:</span>
-        <ul>
-            <li>Pagina Error 404</li>
-            <li>Pagina de contacto</li>
-            <li>...</li>
-        </ul>
-        </div>
-
-        </div>
-        </div>
-
-        {/* INTEGRANTE 4 */}
-        <div className="col-md-3 col-sm-6 team-card">
-        <div div className="team-box">
-
-        <img
-        src={pabloImg}
-        alt="Pablo Padilla"
-        className="team-photo"
-        />
-
-        <h4>Pablo Padilla</h4>
-        <span className="team-role">
-        Frontend Developer
-        </span>
-
-
-        <div className="team-contribution">
-        <span>Mi aporte fue:</span>
-        <ul>
-            <li>Login</li>
-            <li>Logout</li>
-            <li>Register</li>
-            <li>...</li>
-        </ul>
-        </div>
-
-        </div>
-        </div>
-
+            </div>
+          </div>
+        ))}
       </section>
 
     </div>
